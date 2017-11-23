6cb36aabce36063759643a02f3560495a05ffb51
Marie-Louis Neuhoff (uidp3757) <marie-louis.neuhoff@continental-corporation.com>
2017-11-23 11:45:01 +0100

    Scanner eingefügt

--- TestProjekt/src/testproject/TestClass.java
+++ TestProjekt/src/testproject/TestClass.java
@@ -1,10 +1,16 @@
 package testproject;
 
+import java.util.Scanner;
+
 public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Heelo World");
+		int a = 0;
+		System.out.println("Hello World");
+		Scanner sc = new Scanner (System.in);
+		a = sc.nextInt();
+		System.out.println (a);
 	}
 
 }
