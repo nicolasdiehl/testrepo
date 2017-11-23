c242334ba0310af8f532e0b1b122ee9f39fb9eac
CW01\uidp4381 <lalehrouhandeh@yahoo.de>
2017-11-23 12:33:41 +0100

    Test 3 Laleh

--- TestProjekt/src/testproject/TestClass.java
+++ TestProjekt/src/testproject/TestClass.java
@@ -4,7 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Heelo    World");
+		System.out.println("Hello Word");
+		System.out.println("wir sind cool");
 		//neuer Zweig Test
 	}
 
